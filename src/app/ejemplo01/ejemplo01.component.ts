0c7786cf1d08c53ecf9deb6e518023b76d06b748
Luis181996 <luishumberto181996@hotmail.com>
2024-06-19 16:05:29 -0500

    primer commit Ortiz

---
+++ src/app/ejemplo01/ejemplo01.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo01',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejemplo01.component.html',
+  styleUrl: './ejemplo01.component.css'
+})
+export class Ejemplo01Component {
+
+}
